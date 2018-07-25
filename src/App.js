42ca807028aeee1868e6e5263f6248b1345b710b
nivred <emaildervin@gmail.com>
2018-07-25 11:15:55 -0500

    added header and contact components, and installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="ksmith@gmail.com"
+            phone="555-555-1234"
+          />
+        </div>
       </div>
     );
   }
